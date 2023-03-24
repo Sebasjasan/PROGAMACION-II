c87abb9b905977ed31488b4dd1fea4a4e9f7b8af
Sebasjasan <118089634+Sebasjasan@users.noreply.github.com>
2023-03-24 15:12:06 -0500

    Ingreso o retiro de unidades

--- PRUEBA/src/controlproducto.cpp
+++ PRUEBA/src/controlproducto.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 
+string productos[] = {"Producto01", "Producto02", "Producto03", "Producto04", "Producto05", "Producto06", "Producto07", "Producto08", "Producto09", "Producto10"};
+int unidades[] = {100, 15, 55, 288, 319, 421, 423, 201, 38, 128};
+
 void ordenar()
 {
     int productos[] = {8, 4, 5, 9, 1, 2};
@@ -23,8 +26,9 @@ void ordenar()
     }
 }
 
-void buscarProducto(){
-    int producto[] = {1,2,3,4,5,6};
+void buscarProducto()
+{
+    int producto[] = {1, 2, 3, 4, 5, 6};
     int nro;
     cout << "INGRESE UN NUMERO: ";
     cin >> nro;
@@ -33,7 +37,7 @@ void buscarProducto(){
     for (int i = 0; i < 6; i++)
     {
         if (nro == producto[i])
-        band = 'V';
+            band = 'V';
     }
 
     if (band == 'F')
@@ -41,17 +45,58 @@ void buscarProducto(){
 
     if (band == 'V')
         cout << "EL NUMERO SI EXISTE";
-    
-
 }
-void productos(){
-    string productos[2] = {"UNO","DOS"};
-    cout << productos[1];
+void totalProductos()
+{
+    for (int i = 0; i < 10; i++)
+        cout << productos[i] << " " << unidades[i] << endl;
+}
+void ingresarRetirar()
+{
+    char nameProducto[15];
+    int opciones, ingreso, retiro;
+
+    cout << "INGRESAR O RETIRA UNIDADES\n"
+         << "1. [+] INGRESAR\n"
+         << "2. [+] RETIRAR\n"
+         << "OPCIONES: ";
+    cin >> opciones;
+    switch (opciones)
+    {
+    case 1:
+        cout << "ESCRIBA EL NOMBRE DEL PRODUCTO PARA INGESAR UNIDADES: ";
+        cin >> nameProducto;
+        cout << "NUMERO DE UNIDADES A INGRESAR: ";
+        cin >> ingreso;
+        if (nameProducto == productos[0])
+            unidades[0] += ingreso;
+            break;
+    case 2:
+        cout << "ESCRIBA EL NOMBRE DEL PRODUCTO PARA RETITAR UNIDADES: ";
+        cin >> nameProducto;
+        cout << "NUMERO DE UNIDADES A RETIRAR: ";
+        cin >> retiro;
+        if (nameProducto == productos[0])
+            unidades[0] = unidades[0] - retiro;
+
+        break;
+
+    default:
+        break;
+    }
 }
 
 int main()
 {
     // ordenar();
     // buscarProducto();
-    productos();
+    totalProductos();
+    system("pause");
+    system("cls");
+    ingresarRetirar();
+    system("pause");
+    system("cls");
+    totalProductos();
+
+
 }
